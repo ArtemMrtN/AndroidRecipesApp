691578abc300388110621c91282bd3b830ece231
artem <artemmrt@gmail.com>
2024-11-30 08:14:30 +0400

    RA-02 targetSdk, compileSd = 35, agp = "8.6.1"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.mrt.androidrecipesapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mrt.androidrecipesapp"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
