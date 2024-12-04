99e748238d0dc18ed178b664585f308970c983e0
artem <artemmrt@gmail.com>
2024-12-04 20:30:24 +0400

    RA-05 initial comment

--- app/src/main/java/com/mrt/androidrecipesapp/MainActivity.kt
+++ app/src/main/java/com/mrt/androidrecipesapp/MainActivity.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.mrt.androidrecipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
-    private val binding get() = _binding  ?: throw IllegalStateException("binding = null")
+    private val binding get() = _binding ?: throw IllegalStateException("binding = null")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +18,19 @@ class MainActivity : AppCompatActivity() {
 
         supportFragmentManager.commit {
             add<CategoriesListFragment>(R.id.mainContainer)
+        }
+
+        binding.buttonCategory.setOnClickListener {
+            supportFragmentManager.commit {
+                replace<CategoriesListFragment>(R.id.mainContainer)
+                setReorderingAllowed(true)
+            }
+        }
+        binding.buttonFavorites.setOnClickListener {
+            supportFragmentManager.commit {
+                replace<FavoritesFragment>(R.id.mainContainer)
+                setReorderingAllowed(true)
+            }
         }
     }
 }
