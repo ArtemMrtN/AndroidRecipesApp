c34004b8311e2b7824fcb67559663a6fc22e9c11
artem <artemmrt@gmail.com>
2024-12-19 16:10:19 +0400

    RA-17 initial comment

--- app/src/main/java/com/mrt/androidrecipesapp/RecipeFragment.kt
+++ app/src/main/java/com/mrt/androidrecipesapp/RecipeFragment.kt
@@ -1,6 +1,7 @@
 package com.mrt.androidrecipesapp
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
@@ -27,7 +28,7 @@ class RecipeFragment : Fragment() {
         return binding.root
     }
 
-    override fun  onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         val recipe: Recipe = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             arguments?.getParcelable(RecipesListFragment.ARG_RECIPE, Recipe::class.java)
@@ -58,16 +59,24 @@ class RecipeFragment : Fragment() {
         binding.recipesItemImage.setImageDrawable(drawable)
         binding.recipesItemImage.contentDescription = recipe.title
 
-        var isHeartActive = true
-        binding.iconFavorites.setImageResource(R.drawable.ic_heart_empty)
+        val favoritesId = getFavorites()
+
+        if (favoritesId.any { it.toIntOrNull() == recipe.id }) {
+            binding.iconFavorites.setImageResource(R.drawable.ic_heart)
+        } else {
+            binding.iconFavorites.setImageResource(R.drawable.ic_heart_empty)
+        }
+
         binding.iconFavorites.setOnClickListener {
 
-            if (isHeartActive) {
-                binding.iconFavorites.setImageResource(R.drawable.ic_heart)
-            } else {
+            if (favoritesId.any { it.toIntOrNull() == recipe.id }) {
                 binding.iconFavorites.setImageResource(R.drawable.ic_heart_empty)
+                favoritesId.remove(recipe.id.toString())
+            } else {
+                binding.iconFavorites.setImageResource(R.drawable.ic_heart)
+                favoritesId.add(recipe.id.toString())
             }
-            isHeartActive = !isHeartActive
+            saveFavorites(favoritesId)
         }
 
     }
@@ -76,13 +85,14 @@ class RecipeFragment : Fragment() {
 
         binding.rvIngredients.adapter = IngredientsAdapter(recipe.ingredients)
 
-        val divider = MaterialDividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL).apply {
-            isLastItemDecorated = false
-            dividerInsetStart = resources.getDimensionPixelSize(R.dimen.margin_three_fourths)
-            dividerInsetEnd = resources.getDimensionPixelSize(R.dimen.margin_three_fourths)
-            dividerColor = resources.getColor(R.color.light_gray)
-            dividerThickness = resources.getDimensionPixelSize(R.dimen.margin_xxs)
-        }
+        val divider =
+            MaterialDividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL).apply {
+                isLastItemDecorated = false
+                dividerInsetStart = resources.getDimensionPixelSize(R.dimen.margin_three_fourths)
+                dividerInsetEnd = resources.getDimensionPixelSize(R.dimen.margin_three_fourths)
+                dividerColor = resources.getColor(R.color.light_gray)
+                dividerThickness = resources.getDimensionPixelSize(R.dimen.margin_xxs)
+            }
         binding.rvIngredients.addItemDecoration(divider)
 
         binding.rvMethod.adapter = MethodAdapter(recipe.method)
@@ -92,7 +102,9 @@ class RecipeFragment : Fragment() {
 
             @SuppressLint("SetTextI18n")
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                (binding.rvIngredients.adapter as IngredientsAdapter).updateIngredients(seekBar?.progress ?: 1)
+                (binding.rvIngredients.adapter as IngredientsAdapter).updateIngredients(
+                    seekBar?.progress ?: 1
+                )
                 binding.recipeNumberOfServings.text = (progress.takeIf { it >= 0 } ?: 1).toString()
             }
 
@@ -104,6 +116,20 @@ class RecipeFragment : Fragment() {
             }
         })
 
+    }
+
+    private fun saveFavorites(id: Set<String>) {
+        val sharedPrefs = requireActivity().getSharedPreferences("favorites", Context.MODE_PRIVATE)
+        sharedPrefs.edit()
+            .putStringSet("favorites_id", id)
+            .apply()
+    }
+
+    private fun getFavorites(): MutableSet<String> {
+        val sharedPrefs = requireActivity().getSharedPreferences("favorites", Context.MODE_PRIVATE)
+        val storedSet = sharedPrefs.getStringSet("favorites_id", emptySet()) ?: emptySet()
+
+        return HashSet(storedSet)
     }
 
 }
